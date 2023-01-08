9357f0930ebe020de4b3c83e99968ce6eb0dd674
anuj66 <saraogi.anuj9@gmail.com>
2023-01-08 14:39:04 +0530

    Initial Commit

---
+++ inbox-updated/deploy.js
@@ -0,0 +1,29 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { abi, evm } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "meadow favorite replace opinion subway shift desk issue dismiss intact state dynamic",
+  "https://goerli.infura.io/v3/90fca4faf34a45ac93bcc98ae7fdfb98"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("Accounts : ", accounts[0]);
+  const result = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object,
+      arguments: ["Hi there!"],
+    })
+    .send({
+      gas: "1000000",
+      from: accounts[0],
+    });
+
+  console.log("Contract deployed to : ", result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
